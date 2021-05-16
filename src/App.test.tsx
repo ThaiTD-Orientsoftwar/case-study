d4851c7ae45bbf0b1470c2c454a9e1763547a0b6
ThaiTD-Orientsoftwar <thai.tran@orientsoftware.com>
2021-05-16 23:28:04 +0700

    update App

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { render } from "@testing-library/react";
+// import { Provider } from "react-redux";
+// import store from "./app/store";
+// import { shallow } from "enzyme";
+// import App from "./App";
+
+test("renders learn react link", () => {
+  expect(1).toEqual(1);
+});
